40084951082bf794943b99b1aac3a51b15d907c3
Sonja Krause-Harder <skh@rcane.de>
2015-10-15 17:27:23 +0200

    Add first talk, define data structure.

---
+++ js/data.js
@@ -0,0 +1,10 @@
+var talks = [
+  {
+    "speaker": "Suz Hinton",
+    "title": "Refactoring the dinosaur",
+    "conference": "JSConf EU",
+    "year": 2015,
+    "url": "https://www.youtube.com/watch?v=VxxTkhSTJbA"
+    "tags": ["javascript", "hardware"]
+  }, {}
+];
