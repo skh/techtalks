cf45ab0aa373209c008bb8e3b91ca6324df6ae98
Sonja Krause-Harder <skh@rcane.de>
2015-11-22 13:08:57 +0100

    Gulpfile in ES2015

---
+++ gulpfile.babel.js
@@ -0,0 +1,9 @@
+"use strict";
+
+import gulp from 'gulp';
+
+gulp.task('default', () => 
+  console.log("From Gulp with love")
+	//gulp.src('data/**/*.json')
+	//	.pipe(gulp.dest('./build/js'));
+);
--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-
-var gulp = require('gulp');
-
-gulp.task('default', function() {
-	gulp.src('data/**/*.json')
-		.pipe(gulp.dest('./build/js'));
-});
